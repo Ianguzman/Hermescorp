f066bdb40e37dcbc4b1c30b058e3b8d6a0f26d0f
Ian_Guzman <osvaldoianguzman22@gmail.com>
2023-05-29 10:43:49 -0600

    actualizaciondel29/05/2023

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from 'react'; 
 import { CssVarsProvider, useColorScheme } from '@mui/joy/styles';
 import { useState } from 'react';
 import Box from '@mui/joy/Box';
@@ -57,7 +57,64 @@ function SpacingGrid() {
   
 }
 
+const Cuadrospalette = () => {
+  const colors = {
+    blue: ['#005b96', '#0278ae', '#039fb8', '#04c4d9', '#0be2f0', '#0cf0ff'],
+    purple: ['#400080', '#5100a8', '#6300d4', '#7500ff', '#9700ff', '#b900ff'],
+    grey: ['#333333', '#4f4f4f', '#6b6b6b', '#878787', '#a3a3a3', '#bfbfbf'],
+    yellow: ['#ffd700', '#ffdd00', '#ffe400', '#ffeb00', '#fff200', '#fff900'],
+    red: ['#a80000', '#c40000', '#e10000', '#ff0000', '#ff1919', '#ff3333'],
+    green: ['#008000', '#009900', '#00b200', '#00cc00', '#00e500', '#00ff00'],
+  };
 
+  const [color, setColor] = useState('blue');
+
+  const handleColorChange = (event) => {
+    setColor(event.target.value);
+  };
+
+  const changeColor = (newColor) => {
+    setColor(newColor);
+  };
+
+  return (
+    <div>
+      <input type="color" value={colors[color][0]} onChange={handleColorChange} />
+      <h1>Paleta de colores</h1>
+      <div>
+        <button style={{ backgroundColor: colors.blue[700], margin: 10, borderRadius: 20 }} onClick={() => changeColor('blue')}>
+          Primario
+        </button>
+        <button style={{ backgroundColor: colors.purple[700], margin: 10, borderRadius: 20 }} onClick={() => changeColor('purple')}>
+          Secundario
+        </button>
+        <button style={{ backgroundColor: colors.grey[700], margin: 10, borderRadius: 20 }} onClick={() => changeColor('grey')}>
+          Neutro
+        </button>
+        <button style={{ backgroundColor: colors.yellow[700], margin: 10, borderRadius: 20 }} onClick={() => changeColor('yellow')}>
+          Advertencia
+        </button>
+        <button style={{ backgroundColor: colors.red[700], margin: 10, borderRadius: 20 }} onClick={() => changeColor('red')}>
+          Peligro
+        </button>
+        <button style={{ backgroundColor: colors.green[700], margin: 10, borderRadius: 20 }} onClick={() => changeColor('green')}>
+          Éxito
+        </button>
+      </div>
+      <Box display="flex" flexWrap="wrap">
+        {colors[color].map((colorCode, index) => (
+          <Box
+            key={index}
+            width="100px"
+            height="100px"
+            backgroundColor={colorCode}
+            margin="10px"
+          />
+        ))}
+      </Box>
+    </div>
+  );
+};
 
 export default function ColorPicker() {
   const [color, setColor] = useState('#ffffff');
@@ -83,39 +140,21 @@ export default function ColorPicker() {
       display: 'grid',
       justifyItems: 'center',
       alignItems: 'center',
+      
 
     }}
   >
-
+    
     <ModeSwitcher/>
-      <input type="color" value={color} onChange={handleColorChange} />
-      <h1>Paleta de colores</h1>
-      <div>
-        <button style={{ backgroundColor: color }} onClick={() => changeColor(colors.blue[700])}>
-          Primario
-        </button>
-        <button style={{ backgroundColor: color }} onClick={() => changeColor(colors.purple[700])}>
-          Secundario
-        </button>
-        <button style={{ backgroundColor: color }} onClick={() => changeColor(colors.grey[700])}>
-          Neutro
-        </button>
-        <button style={{ backgroundColor: color }} onClick={() => changeColor(colors.yellow[700])}>
-          Advertencia
-        </button>
-        <button style={{ backgroundColor: color }} onClick={() => changeColor(colors.red[700])}>
-          Peligro
-        </button>
-        <button style={{ backgroundColor: color }} onClick={() => changeColor(colors.green[700])}>
-          Éxito
-        </button>
-      </div>
+
+      <Cuadrospalette/>
       
       <Grid sx={{ flexGrow: 2 }} container>
 
             <Grid item xs={3.5}>
               <Grid container justifyContent="center">
                 <List>
+                  
                   <Botonnorm />
                 </List>
                 <List>
@@ -144,7 +183,7 @@ export default function ColorPicker() {
                 </List>
 
               </Grid>
-            </Grid>
+            </Grid> 
     </Grid>
 
         <Divider style={{margin:20}}/>
